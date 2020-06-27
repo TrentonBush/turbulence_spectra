a09abe7257b8e24552473845d9fe7748c1052dd7
t-desktop <tpbush@gmail.com>
2020-06-27 00:24:53 -0700

    draft sonic_summary()

--- src/data/process_matlab.py
+++ src/data/process_matlab.py
@@ -4,14 +4,14 @@ import numpy as np
 from pathlib import Path
 
 
-def matlab_to_pandas(filepath: Path):
+def matlab_to_pandas(filepath: Path) -> pd.DataFrame:
     exclusions = {
-            "__header__",
-            "__version__",
-            "__globals__",
-            "tower",
-            "datastream",
-        }
+        "__header__",
+        "__version__",
+        "__globals__",
+        "tower",
+        "datastream",
+    }
     matlab = loadmat(filepath, squeeze_me=True)
 
     sample_freq = matlab["tower"]["daqfreq"].item()
@@ -29,3 +29,27 @@ def matlab_to_pandas(filepath: Path):
 
     return df
 
+
+def sonic_summary(df: pd.DataFrame, height: str):
+    raise NotImplementedError  # need spectral density functions
+    out = {}
+    horizontal = f"Sonic_CupEqHorizSpeed_{height}m"
+    vertical = f"Sonic_z_clean_{height}m"
+    direction = f"Sonic_direction_{height}m"
+
+    out["nan_count"] = df[horizontal].isna().sum()
+    out["mean"] = df[horizontal].mean()
+    out["mean_square"] = df[horizontal].pow(2).mean()
+    out["mean_cube"] = df[horizontal].pow(3).mean()
+
+    out["nan_count_vert"] = df[vertical].isna().sum()
+    out["mean_vert"] = df[vertical].mean()
+    out["mean_square_vert"] = df[vertical].pow(2).mean()
+    out["mean_cube_vert"] = df[vertical].pow(3).mean()
+
+    out["mean_dir"] = df[direction].mean()
+    out["waked_frac"] = (
+        df[[direction]].query(f"80 < {direction} < 210").count()[0] / 12000
+    )
+
+    return out
