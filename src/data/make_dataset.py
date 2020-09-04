56ef87318c1e9e2fc5f86d83d91ae27def4cb445
t-desktop <tpbush@gmail.com>
2020-09-04 14:03:06 -0700

    add multiprocessing and CLI

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
-from typing import Union, Dict
+from typing import Optional, Tuple, Sequence, Dict
 import pandas as pd
+from tqdm import tqdm
+from multiprocessing import Pool
+import logging
+import typer
+
 import src.data.process_matlab as pmat
 
 SONIC_HEIGHTS = (119, 100, 74, 61, 41, 15)
@@ -19,10 +24,10 @@ CUP_NAMES = (
 )
 
 
-def summarize_file(filepath: Path,) -> Dict[str, pd.DataFrame]:
+def summarize_file(filepath: Path,) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     timestamp = pd.to_datetime(
         filepath.name[:16], format="%m_%d_%Y_%H_%M"
-    ) - pd.Timedelta(-7, unit="h")
+    ) + pd.Timedelta(pmat.EXPECTED_UTC_OFFSET, unit="h")
     matlab_df = pmat.matlab_to_pandas(filepath)
     sonics = []
     cups = []
@@ -33,38 +38,59 @@ def summarize_file(filepath: Path,) -> Dict[str, pd.DataFrame]:
         cups.append(pd.DataFrame(pmat.cup_summary(matlab_df, cup), index=[0]))
 
     sonics = pd.concat(sonics, ignore_index=True)
-    sonics["timestamp"] = timestamp
+    sonics["timestamp"] = timestamp  # type: ignore
     cups = pd.concat(cups, ignore_index=True)
-    cups["timestamp"] = timestamp
+    cups["timestamp"] = timestamp  # type: ignore
 
     misc = pd.DataFrame(pmat.misc_summary(matlab_df), index=[0])
     misc["timestamp"] = timestamp
 
-    return {"sonics": sonics, "cups": cups, "misc": misc}
+    return (sonics, cups, misc)
 
 
-def main(source_dir: Union[Path, str], out_dir: Union[Path, str]) -> None:
+def summarize_many_multiprocess(
+    input_files: Sequence[Path], n_processes: Optional[int] = None,
+) -> Dict[str, pd.DataFrame]:
+    count = len(input_files)
+    logging.info(f"Processing {count} files")
+    print(f"Processing {count} files")
+    start = pd.Timestamp("now")
+    with Pool(processes=n_processes) as pool:
+        processed_files = list(
+            tqdm(
+                pool.imap(summarize_file, input_files, chunksize=10),
+                total=len(input_files),
+            )
+        )
+    combined = map(pd.concat, zip(*processed_files))
+    out_dict = dict(zip(["sonic", "cup", "misc"], combined))
+    end = pd.Timestamp("now")
+    duration = (end - start).round("s")
+    message = "\n".join(
+        [
+            f"Files: {count}",
+            f"Elapsed time: {duration}",
+            f"Seconds per file: {(duration / count).total_seconds():.3f}",
+        ]
+    )
+    logging.info(message)
+    print(message)
+    return out_dict
+
+
+def main(source_dir: str, out_dir: str) -> None:
+    logging.basicConfig(
+        filename=Path(out_dir) / "process.log",
+        format="%(asctime)s %(message)s",
+        level=logging.INFO,
+    )
     input_files = list(Path(source_dir).glob("*.mat"))
-
-    # initialize - convert dict of dfs to dict of lists of dfs
-    out_dfs = summarize_file(input_files[0])
-    out_dfs = dict(zip(out_dfs.keys(), map(lambda x: [x], out_dfs.values())))
-
-    for file in input_files[1:]:
-        dfs = summarize_file(file)
-        for key in dfs.keys():
-            out_dfs[key].append(dfs[key])
+    out_dfs = summarize_many_multiprocess(input_files)
 
     for key, val in out_dfs.items():
         filename = Path(out_dir) / f"{key}.parquet"
-        pd.concat(val, ignore_index=True).to_parquet(filename, index=False)
+        val.to_parquet(filename, index=False)
 
 
 if __name__ == "__main__":
-    start = pd.Timestamp("now")
-    source_dir = Path("./data/raw/")
-    assert source_dir.exists()
-    out_dir = Path("./data/processed/")
-    assert out_dir.exists()
-    main(source_dir, out_dir)
-    print(f"Duration: {(pd.Timestamp('now') - start).round('1s')}")
+    typer.run(main)
