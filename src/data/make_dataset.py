ad26cb961a7daea6f9e08a14bef360c955692371
t-desktop <tpbush@gmail.com>
2020-09-06 16:28:05 -0700

    fix multiprocessing logging

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,8 +2,9 @@
 from pathlib import Path
 from typing import Optional, Tuple, Sequence, Dict
 import pandas as pd
+from scipy.io.matlab.miobase import MatReadError
 from tqdm import tqdm
-from multiprocessing import Pool
+import multiprocessing
 import logging
 import typer
 
@@ -24,18 +25,49 @@ CUP_NAMES = (
 )
 
 
+def create_logger():
+    logger = multiprocessing.get_logger()
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        "[%(asctime)s| %(levelname)s| %(processName)s] %(message)s"
+    )
+    handler = logging.FileHandler(Path("./data/processed/process.log"))
+    handler.setFormatter(formatter)
+
+    # prevents duplicated messages
+    if not len(logger.handlers):
+        logger.addHandler(handler)
+    return logger
+
+
 def summarize_file(filepath: Path,) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    logger = create_logger()
     timestamp = pd.to_datetime(
         filepath.name[:16], format="%m_%d_%Y_%H_%M"
     ) + pd.Timedelta(pmat.EXPECTED_UTC_OFFSET, unit="h")
-    matlab_df = pmat.matlab_to_pandas(filepath)
+    try:
+        matlab_df = pmat.matlab_to_pandas(filepath)
+    except (OSError, MatReadError, ValueError) as e:
+        message = f"Skipped {filepath.name} due to load error"
+        tqdm.write(message)
+        logger.exception(message)
+        logger.exception(e)
+        return tuple([pd.DataFrame()] * 3)  # empty
     sonics = []
     cups = []
-
-    for height in SONIC_HEIGHTS:
-        sonics.append(pd.DataFrame(pmat.sonic_summary(matlab_df, height), index=[0]))
-    for cup in CUP_NAMES:
-        cups.append(pd.DataFrame(pmat.cup_summary(matlab_df, cup), index=[0]))
+    try:
+        for height in SONIC_HEIGHTS:
+            sonics.append(
+                pd.DataFrame(pmat.sonic_summary(matlab_df, height), index=[0])
+            )
+        for cup in CUP_NAMES:
+            cups.append(pd.DataFrame(pmat.cup_summary(matlab_df, cup), index=[0]))
+    except ValueError as e:
+        message = f"Skipped {filepath.name} due to processing error"
+        tqdm.write(message)
+        logger.exception(message)
+        logger.exception(e)
+        return tuple([pd.DataFrame()] * 3)  # empty
 
     sonics = pd.concat(sonics, ignore_index=True)
     sonics["timestamp"] = timestamp  # type: ignore
@@ -51,11 +83,12 @@ def summarize_file(filepath: Path,) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Data
 def summarize_many_multiprocess(
     input_files: Sequence[Path], n_processes: Optional[int] = None,
 ) -> Dict[str, pd.DataFrame]:
+    logger = create_logger()
     count = len(input_files)
-    logging.info(f"Processing {count} files")
-    print(f"Processing {count} files")
+    logger.info(f"Processing {count} files")
+    tqdm.write(f"Processing {count} files")
     start = pd.Timestamp("now")
-    with Pool(processes=n_processes) as pool:
+    with multiprocessing.Pool(processes=n_processes) as pool:
         processed_files = list(
             tqdm(
                 pool.imap(summarize_file, input_files, chunksize=10),
@@ -66,24 +99,13 @@ def summarize_many_multiprocess(
     out_dict = dict(zip(["sonic", "cup", "misc"], combined))
     end = pd.Timestamp("now")
     duration = (end - start).round("s")
-    message = "\n".join(
-        [
-            f"Files: {count}",
-            f"Elapsed time: {duration}",
-            f"Seconds per file: {(duration / count).total_seconds():.3f}",
-        ]
-    )
-    logging.info(message)
-    print(message)
+    message = f"{count} files processed in {duration}. Seconds per file: {(duration / count).total_seconds():.3f}"
+    logger.info(message)
+    tqdm.write(message)
     return out_dict
 
 
 def main(source_dir: str, out_dir: str) -> None:
-    logging.basicConfig(
-        filename=Path(out_dir) / "process.log",
-        format="%(asctime)s %(message)s",
-        level=logging.INFO,
-    )
     input_files = list(Path(source_dir).glob("*.mat"))
     out_dfs = summarize_many_multiprocess(input_files)
 
@@ -93,4 +115,6 @@ def main(source_dir: str, out_dir: str) -> None:
 
 
 if __name__ == "__main__":
+    logger = create_logger()
+    logger.info("Starting")
     typer.run(main)
